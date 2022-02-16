46590bd0e9a2efb46a15b9012088468493a870c4
anfepema <anfepema700>
2022-02-15 19:08:04 -0800

    betty all main, print, all files

--- sort.h
+++ sort.h
@@ -1,22 +1,22 @@
 #ifndef _SORT_H_
 #define _SORT_H_
- 
+
 #include <stddef.h>
- 
+
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
- const int n;
- struct listint_s *prev;
- struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
- 
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
@@ -31,5 +31,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
- 
+
 #endif /* _SORT_H_ */
